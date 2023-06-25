d79a3a31420a62b16a967035fbae0639dd7005a8
Tocco17 <fede.ruocco@gmail.com>
2023-06-25 12:39:34 +0200

    update FE: RouteComponent
    added route enum and login action

--- portafoglio.react/src/contextes/route.context.tsx
+++ portafoglio.react/src/contextes/route.context.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Login from "../pages/login";
+import Login, { loginSubmitAction } from "../pages/login";
 import Home from "../pages/home";
 
 const router = createBrowserRouter([
@@ -14,13 +14,28 @@ const router = createBrowserRouter([
                 path: 'auth/',
                 children: [
                     {
-                        path: 'login',
-                        element: <Login />
+                        path: 'login/',
+                        action: loginSubmitAction,
+                        element: <Login />,
+                        children: [
+                            {
+                                path: 'search',
+                                action: loginSubmitAction,
+                                errorElement: <div>Oops</div>
+                            }
+                        ],
                     }
                 ]
             }
         ]
-    }
+    },
+    
 ])
 
 export default router
+
+
+export enum route {
+    home = '/',
+    login = '/auth/login',
+}
