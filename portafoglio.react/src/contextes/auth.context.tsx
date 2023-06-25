7d8500f36d9225aeceb72983b97d0abdb378bece
Tocco17 <fede.ruocco@gmail.com>
2023-06-25 12:37:46 +0200

    clean FE: Auth Context

--- portafoglio.react/src/contextes/auth.context.tsx
+++ portafoglio.react/src/contextes/auth.context.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactElement, useContext, useState } from "react";
+import { createContext, useContext } from "react";
 import User from "../models/entities/user.interface";
 
 export interface IAuth {
