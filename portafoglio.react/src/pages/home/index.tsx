728677708791304f735790040c77e37ee5739707
Tocco17 <fede.ruocco@gmail.com>
2023-06-25 12:38:42 +0200

    update FE: home
    Change Link to Button component=Link

--- portafoglio.react/src/pages/home/index.tsx
+++ portafoglio.react/src/pages/home/index.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
+import { route } from "../../contextes/route.context"
+import Button from '@mui/material/Button';
+
 const Home = () => {
     return (
         <>
         <div>Home</div>
+        <Button component={Link} to={route.login}>Login</Button>
         </>
     )
 }
