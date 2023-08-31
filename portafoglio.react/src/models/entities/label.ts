408359d4a4ca45b77264534ed3f6b72c96a19dfa
FedericoRuocco <federico.ruocco@giuneco.it>
2023-08-31 22:30:15 +0200

    label entity has properties

--- portafoglio.react/src/models/entities/label.ts
+++ portafoglio.react/src/models/entities/label.ts
@@ -1,5 +1,8 @@
 import { LogicDeleteEntity } from "./base-entity";
 
 export interface Label extends LogicDeleteEntity {
-
+	idEarningSuddivision: number
+	name: string
+	description?: string
+	idFatherLabel?: number
 }
