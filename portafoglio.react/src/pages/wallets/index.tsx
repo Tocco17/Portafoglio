2e13eb199e9ede012e2885fdeedf198da24f9408
FedericoRuocco <federico.ruocco@giuneco.it>
2023-09-05 16:26:49 +0200

    clean comment

--- portafoglio.react/src/pages/wallets/index.tsx
+++ portafoglio.react/src/pages/wallets/index.tsx
@@ -69,32 +69,3 @@ export const Wallets = () => {
 		</>
 	)
 }
-
-/*
-TODO - WALLET
-L'idea da implementare:
-
-Pagina 'wallets'
-	Cosa si vede:
-		Lista di wallet
-		Barra di ricerca per filtrare i wallet
-		Bottone New wallet
-	Azioni
-		Filtrare la lista -> va alla pagina 'wallets?q=[ricerca]'
-		Cliccare su un wallet -> va alla pagina 'wallets/[idWallet]'
-		Creare un nuovo bottone -> va alla pagina 'wallets/new'
-
-Pagina 'wallets/[idWallet]'
-	Cosa si vede
-		Barra laterale uguale alla pagina 'wallets' con anche le stesse azioni disponibili
-		Tutti i dettagli del wallet selezionato
-		Se non è disponibile pagina di errore
-		
-Pagina 'wallets/new'
-	Cosa si vede
-		Barra laterale uguale alla pagina 'wallets' con anche le stesse azioni disponibili
-		Form per creare un nuovo wallet
-
-		
-
-*/
