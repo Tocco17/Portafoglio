385dee00d9e967e78ce570c710e42d2c152d4659
FedericoRuocco <federico.ruocco@giuneco.it>
2023-09-29 11:20:45 +0200

    api deleted

--- portafoglio.api/Entities/BaseLogicDeleteEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace portafoglio.api.Entities;
-
-public class BaseLogicDeleteEntity: BaseEntity
-{
-	/// <summary>
-	/// Check if it's active or not
-	/// </summary>
-	public bool Active { get; set; }
-}
--- portafoglio.api/Models/Filters/IFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿using portafoglio.api.Entities;
-
-namespace portafoglio.api.Models.Filters;
-
-public interface IFilter<TEntity>
-	where TEntity : BaseEntity
-{
-	public IQueryable<TEntity> GetFilteredQuery(IQueryable<TEntity> baseQuery);
-}
