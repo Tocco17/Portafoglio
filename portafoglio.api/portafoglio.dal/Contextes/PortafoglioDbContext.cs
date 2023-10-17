b8e1a912e494b1c2ba9404460dc26eb9785c11e0
FedericoRuocco <federico.ruocco@giuneco.it>
2023-10-17 15:07:53 +0200

    clean

--- portafoglio.api/portafoglio.dal/Contextes/PortafoglioDbContext.cs
+++ portafoglio.api/portafoglio.dal/Contextes/PortafoglioDbContext.cs
@@ -19,16 +19,3 @@ public class PortafoglioDbContext : DbContext
 	public virtual DbSet<Wallet> Wallets { get; set; } = null!;
 
 }
-
-
-public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PortafoglioDbContext>
-{
-	public PortafoglioDbContext CreateDbContext(string[] args)
-	{
-		IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../MyCookingMaster.API/appsettings.json").Build();
-		var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-		var connectionString = configuration.GetConnectionString("DatabaseConnection");
-		builder.UseSqlServer(connectionString);
-		return new ApplicationDbContext(builder.Options);
-	}
-}
